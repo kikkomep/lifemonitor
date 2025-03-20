b194d2e4489172fc69cf9e2d18cd5ef8d8a70f34
Marco Enrico Piras <kikkomep@crs4.it>
2025-03-20 15:22:07 +0100

    feat(testing): :sparkles: switch to the production `seek` deployment

--- tests/config/registries/seek/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -Rf /seek/filestore/*
-cp -a data/filestore/* /seek/filestore/
-cp -a data/db.sqlite3 /seek/sqlite3-db/production.sqlite3
-
-docker/upgrade.sh
-docker/entrypoint.sh
