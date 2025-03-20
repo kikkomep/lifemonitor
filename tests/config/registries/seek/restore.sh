1f9df7d31a21c609b55c927d64c4b25c2ab2c8e5
Marco Enrico Piras <kikkomep@crs4.it>
2025-03-20 18:31:22 +0100

    refactor(testing): :recycle: restore seek data from seek-data image

--- tests/config/registries/seek/restore.sh
+++ tests/config/registries/seek/restore.sh
@@ -1,3 +1,23 @@
+# Copyright (c) 2020-2024 CRS4
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 #!/bin/bash
 
 CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -18,10 +38,18 @@ docker volume rm $DB_VOLUME_NAME
 docker volume rm $FILESTORE_VOLUME_NAME
 docker volume create --name=$DB_VOLUME_NAME
 docker volume create --name=$FILESTORE_VOLUME_NAME
+# Start the containers without starting the services
 docker compose ${COMPOSE_FILE_OPTION} up --no-start
-docker run --rm --volumes-from seek -v "${CURRENT_PATH}":/backup ubuntu bash -c "tar xfv /backup/seek-filestore.tar"
-docker run --rm --volumes-from seek-mysql -v "${CURRENT_PATH}":/backup ubuntu bash -c "tar xfv /backup/seek-mysql-db.tar"
+# Restore the data
+# docker run --rm --volumes-from seek -v "${CURRENT_PATH}":/backup ubuntu bash -c "tar xfv /backup/seek-filestore.tar"
+# docker run --rm --volumes-from seek-mysql -v "${CURRENT_PATH}":/backup ubuntu bash -c "tar xfv /backup/seek-mysql-db.tar"
+docker run --rm --volumes-from seek-data --volumes-from seek-data ubuntu \
+    bash -c "tar xfv /seek-data/seek-filestore.tar -C /seek --strip-components=1"
+docker run --rm  --volumes-from seek-mysql --volumes-from seek-data ubuntu \
+    bash -c "tar xfv /seek-data/seek-mysql-db.tar -C /var --strip-components=1"
+
+# Start the main containers
 docker compose ${COMPOSE_FILE_OPTION} up -d
 
-
+# Reindex the data
 docker exec seek bundle exec rake seek:reindex_all
