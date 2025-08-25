ea826d3b466504e6b18b41e4d97aa8457d2c65aa
Marco Enrico Piras <kikkomep@crs4.it>
2025-08-25 04:56:22 +0000

    feat(github-service): :zap: add utility function to extract workflow params from s test instance

--- lifemonitor/api/models/services/github/utils.py
+++ lifemonitor/api/models/services/github/utils.py
@@ -26,6 +26,9 @@ from typing import Tuple
 from urllib.error import URLError
 from urllib.parse import urlparse
 
+import github
+
+from lifemonitor.api.models.testsuites.testinstance import TestInstance
 import lifemonitor.exceptions as lm_exceptions
 
 # set module level logger
@@ -77,3 +80,70 @@ def parse_workflow_url(resource: str) -> Tuple[str, str, str]:
         raise lm_exceptions.SpecificationNotValidException(
             detail="Unexpected format of link to Github Workflow",
             parse_error=e.args[0])
+
+
+def strip_remote_prefix(shorthand: str) -> str:
+    """
+    Remove remote prefix (e.g. 'origin/', 'upstream/') from a branch shorthand.
+    If no prefix is present, return shorthand unchanged.
+    """
+    if "/" in shorthand:
+        remote, branch = shorthand.split("/", 1)
+        return branch
+    return shorthand
+
+
+def get_workflow_params(test_instance: TestInstance) -> tuple:
+    """
+    Returns the workflow parameters for the given test instance.
+    This method is a placeholder and should be implemented in subclasses.
+    """
+    # This method should be overridden in subclasses to provide specific parameters
+    branch = github.GithubObject.NotSet
+    tag = github.GithubObject.NotSet
+    created = github.GithubObject.NotSet
+    # Init reference to the revision
+    revision = github.GithubObject.NotSet
+    try:
+        revision = test_instance.test_suite.workflow_version.repository.revision
+        if revision:
+            created = revision.created.isoformat()
+            if revision.is_branch():
+                branch = strip_remote_prefix(revision.main_ref.shorthand)
+                logger.debug("Branch found: %s", branch)
+            elif revision.is_tag():
+                tag = strip_remote_prefix(revision.main_ref.shorthand)
+                logger.debug("Tag found: %s", tag)
+            else:
+                logger.debug("Revision is neither a branch nor a tag: %s", revision.shorthand)
+        else:
+            logger.debug("No revision found for workflow version %r", test_instance.test_suite.workflow_version)
+
+    except Exception as e:
+        logger.exception("Error retrieving workflow parameters for test instance %s", test_instance.uuid)
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug("Exception details: %s", e)
+
+    # Fallback to the workflow version creation date range if no branch or tag is found
+    try:
+        if not revision or revision is github.GithubObject.NotSet:
+            logger.debug("No branch or tag found for revision %r", revision)
+            workflow_version = test_instance.test_suite.workflow_version
+            logger.debug("Checking Workflow version: %r (previous: %r, next: %r)",
+                         workflow_version, workflow_version.previous_version, workflow_version.next_version)
+            if workflow_version.previous_version and workflow_version.next_version:
+                created = "{}..{}".format(workflow_version.created.isoformat(),
+                                          workflow_version.next_version.created.isoformat())
+            elif workflow_version.previous_version:
+                created = ">={}".format(workflow_version.created.isoformat())
+            elif workflow_version.next_version:
+                created = "<{}".format(workflow_version.next_version.created.isoformat())
+            else:
+                logger.debug("No previous version found, then no filter applied... Loading all available builds")
+    except Exception as e:
+        logger.exception("Error retrieving workflow parameters for test instance %s", test_instance.uuid)
+        raise lm_exceptions.SpecificationNotValidException(
+            detail="Error retrieving workflow parameters",
+            original_exception=str(e))
+
+    return branch, tag, created
