b1d5b9eae0a111d1f05c3b7aeba6d18870494d44
Marco Enrico Piras <kikkomep@crs4.it>
2026-01-18 18:52:03 +0100

    refactor(controller): :recycle: allow reuse of logic to extract a list of scoped workflows

--- lifemonitor/api/controllers.py
+++ lifemonitor/api/controllers.py
@@ -108,54 +108,52 @@ def workflow_registries_get_current():
     return lm_exceptions.report_problem(401, "Unauthorized")
 
 
+def _get_workflows_list_(page: int = 1, per_page: Optional[int] = None, max_items: Optional[int] = None,
+                         subscriptions: bool = False, only_subscriptions: bool = False):
+    page_info = PaginationInfo(page=page, per_page=per_page, max_items=max_items)
+    workflows = []
+    if current_user and not current_user.is_anonymous:
+        logger.info("Getting workflows for user %s", current_user)
+        workflows.extend(lm.get_user_workflows(
+            current_user,
+            include_subscriptions=subscriptions,
+            include_public=True,
+            only_subscriptions=only_subscriptions,
+            page=page_info
+        ))
+    elif current_registry:
+        logger.info("Getting workflows for registry %s", current_registry)
+        workflows.extend(lm.get_registry_workflows(
+            current_registry, include_public=True,
+            page=page_info
+        ))
+    else:
+        logger.info("Getting public workflows")
+        workflows.extend(lm.get_public_workflows(page=page_info))
+    return list(dict.fromkeys(workflows)), page_info
+
+
 @cached(timeout=Timeout.REQUEST)
 def workflows_get(status=False, versions=False, subscriptions=False,
                   page: int = 1, per_page: Optional[int] = None,
                   max_items: Optional[int] = None,
                   only_subscriptions=False,):
-    # Prepare pagination info
-    page_info = PaginationInfo(page=page, per_page=per_page, max_items=max_items)
-    # Collect workflows depending on the current user/registry in session
-    workflows = []
-    # If a user is logged in
-    if current_user and not current_user.is_anonymous:
-        logger.info("Getting workflows for user %s", current_user)
-        workflows.extend(lm.get_user_workflows(current_user,
-                                               include_subscriptions=subscriptions,
-                                               include_public=True,
-                                               only_subscriptions=only_subscriptions,
-                                               page=page_info))
-        logger.info("Total workflows for user %s: %d", current_user, len(workflows))
-    # If a registry is in the current session,
-    # get both public and registry workflows
-    elif current_registry:
-        logger.info("Getting workflows for registry %s", current_registry)
-        workflows.extend(lm.get_registry_workflows(
-            current_registry, include_public=True,
-            page=page_info))
-    # Engage public workflows only
-    else:
-        logger.info("Getting public workflows")
-        workflows.extend(lm.get_public_workflows(page=page_info))
+    workflows, page_info = _get_workflows_list_(page, per_page, max_items,
+                                                subscriptions, only_subscriptions)
 
     logger.debug("workflows_get. Got %s workflows (user: %s)", len(workflows), current_user)
 
-    # Determine whose subscriptions to include
     subscriptions_of = []
     if subscriptions and current_user and not current_user.is_anonymous:
         subscriptions_of = [current_user]
 
-    # Create the serializer
     serializer = serializers.ListOfWorkflows(
         workflow_status=status,
         workflow_versions=versions,
         subscriptionsOf=subscriptions_of,
         page=page_info
     )
-
-    # Remove duplicates and serialize the workflows
-    unique_workflows = list(dict.fromkeys(workflows))
-    return serializer.dump(unique_workflows)
+    return serializer.dump(workflows)
 
 
 def __get_workflow_version__(wf_uuid, wf_version=None) -> models.WorkflowVersion:
